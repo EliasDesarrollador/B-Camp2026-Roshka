83041385292bc08fef3235ae7255b315fc475fb1
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-06 09:55:05 -0300

    feat(EjercicioJava001) add Ejercicio 3

---
+++ EjercicioJava001/src/operaciones/Ejercicio3.java
@@ -0,0 +1,13 @@
+package operaciones;
+
+
+// Ejercicio 3 - Manejo de String 
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		String nombre = "Elías ";
+		System.out.println("Hola Bienvenido !!"+ " "  + nombre);
+
+	}
+
+}
