8b6d8b422dc3bd1cddc7d19e74776f8acfb06017
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-06 09:18:36 -0300

    feat(EjercicioJava001) add Ejercicio 1

---
+++ EjercicioJava001/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EjercicioJava001 {
+}
---
+++ EjercicioJava001/src/operaciones/Ejercicio1.java
@@ -0,0 +1,20 @@
+package operaciones;
+
+//Ejercicio 1 - Operaciones aritmeticas  con dos variables
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		
+		 int num1 = 24 ;
+		 int num2 = 8 ; 
+		
+		System.out.println("Suma : " + (num1 + num2));
+		System.out.println("Resta : " + (num1 - num2));
+		System.out.println("Multiplicacion : " + (num1 * num2));
+		System.out.println("Division : " + (num1 / num2));
+		System.out.println("Modulo : " + (num1 % num2));
+
+	}
+
+}
