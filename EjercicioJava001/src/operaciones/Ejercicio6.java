a1e154108231c5022accc5593c73b9e644af9d66
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-06 10:35:18 -0300

    feat(EjercicioJava001) add Ejercicio 6

---
+++ EjercicioJava001/src/operaciones/Ejercicio6.java
@@ -0,0 +1,15 @@
+package operaciones;
+
+// Ejercicio 6 - Mostrar numeros del 1 al 100 y calcular si son divisibles entre 2 y 3 
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++);{
+			if (i % 2 == 0 && i % 3 == 0);{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
