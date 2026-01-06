c5e4920c7b10fc7e7a42b91bb21f96ec9e9330c8
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-06 10:03:17 -0300

    refactor(EjercicioJava001) add Ejercicio 3

--- EjercicioJava001/src/operaciones/Ejercicio3.java
+++ EjercicioJava001/src/operaciones/Ejercicio3.java
@@ -1,12 +1,20 @@
 package operaciones;
 
+import java.util.Scanner;
+
+
 
 // Ejercicio 3 - Manejo de String 
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		String nombre = "Elías ";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresa tu nombre :");
+		
+		String nombre = sc.nextLine();
 		System.out.println("Hola Bienvenido !!"+ " "  + nombre);
+		sc.close();
 
 	}
 
