7eb9c97808edf9fbc7cfeca02e46da9be94e6f44
Elias Ortiz <elortiz@uaa.edu.py>
2026-01-07 09:35:53 -0300

    feat(EjercicioPoker) add Principal

--- EjercicioPoker/src/poker/Princicpal.java
+++ EjercicioPoker/src/poker/Princicpal.java
@@ -1,9 +1,22 @@
 package poker;
 
+import java.util.Scanner;
+
 public class Princicpal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese 5 cartas (ej: AS  KH  QD  JC  10S):");
+		
+		String[] entrada = scanner.nextLine().split("\\s+");
+		
+		try {
+			ManoPoker mano = new ManoPoker(entrada);
+			System.out.println("Jugada: " + mano.identificarMano());
+		} catch (Exception e ) {
+			System.out.println("Error : " + e.getMessage());
+		}
 
 	}
 
